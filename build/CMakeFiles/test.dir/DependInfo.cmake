b99af765badf313f697aac9f688cce7436fbf732
aleksandrakkot <72742669+aleksandrakkot@users.noreply.github.com>
2020-12-09 19:26:28 +0100

    Add files via upload

---
+++ build/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/aleks/Desktop/Lab3/src/Lab3.cpp" "C:/Users/aleks/Desktop/Lab3/build/CMakeFiles/test.dir/src/Lab3.cpp.obj"
+  "C:/Users/aleks/Desktop/Lab3/src/Menu.cpp" "C:/Users/aleks/Desktop/Lab3/build/CMakeFiles/test.dir/src/Menu.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././src"
+  ".././include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/src/Lab3.cpp.obj"
+  "CMakeFiles/test.dir/src/Menu.cpp.obj"
+  "libtest.dll.a"
+  "test.exe"
+  "test.exe.manifest"
+  "test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
